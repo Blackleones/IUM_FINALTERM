58cc248b135484dc218c91b44911d12ff12cace6
Leonardo Lurci <leonardo.lurci@gmail.com>
2016-06-29 03:06:10 +0200

    finite progress bar cpu

--- animation_test/animation.js
+++ animation_test/animation.js
@@ -1,44 +1,28 @@
 var DEBUG = false;
 
-function incWidth(pb, callback){
-    var text = (pb.getElementsByTagName('span')[1]);
-    var span = (pb.getElementsByClassName('progress')[0]).getElementsByTagName('span')[0];
-    
-    if(span.style.width == "100%"){
-        callback();    
-    }else{
-        var width = parseInt((span.style.width).replace('%', ''));
-        
-        if(width > 70)
-            span.style.background = "linear-gradient(to right, rgba(255,255,22,1), rgba(255,255,22,1))";
-        
-        if(width > 90)
-            span.style.background = "linear-gradient(to right, rgba(229,61,0,1), rgba(229,61,0,1))";
-        
-        if(DEBUG)
-            console.log(width);
-        
-        text.innerHTML = width + 1 + '%';
-        span.style.width = width + 1 + '%';            
-    }
-}
-
 function main(){
-    var pb = document.getElementById('cpu-1');
-
-    /*
-        il thread avvia una funziona aninoma che
-        mi serve per poter avviare la funzione incWidth 
-        passandogli 2 parametri:
-            lo span da elaborare
-            la funzione callback da chiamare per terminare
-            il thread
-    */
-    var pbar = setInterval(function(){
-        incWidth(pb, function(pbar){
-            clearInterval(pbar);
-        });
-    }, 200);
+    var pb = [
+        document.getElementById('cpu-1'),
+        document.getElementById('cpu-2'),
+        document.getElementById('cpu-3'),
+        document.getElementById('cpu-4')
+    ];
+    
+    console.log("avvio");
+    
+    setInterval(function(){
+        for(var cpu in pb){
+           cpu = pb[cpu];
+           cpuprog = (cpu.getElementsByClassName('progress')[0]).getElementsByTagName('span')[0];
+           cputext = (cpu.getElementsByTagName('span')[1]);
+           var rand = Math.floor(Math.random()*100);
+           var color = rand <= 70 ? "rgba(0,255,22,1)": rand < 90 ? "rgba(255,255,22,1)" : "rgba(229,61,0,1)";
+           cpuprog.style.background = color;
+           cpuprog.style.width = rand+"%";
+           cputext.innerHTML = rand+"%";
+        }
+    }, 3000);
+    
 }
 
 main();
